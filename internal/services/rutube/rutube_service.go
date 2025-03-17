cb11e61630368e13a882db0d0b9bd1efcebb8061
Kovalev Pavel <pavelkovalev1605@gmail.com>
2025-03-17 10:43:29 +0700

    refactor(downloader): refactored methods names, logs

--- internal/services/rutube/rutube_service.go
+++ internal/services/rutube/rutube_service.go
@@ -2,7 +2,7 @@ package rutube
 
 import "github.com/EnOane/cli_downloader/internal/lib"
 
-func Download(videoUrl, destPath string) (string, error) {
+func DownloadAndSave(videoUrl, destPath string) (string, error) {
 	return lib.DownloadAndSave(videoUrl, destPath)
 }
 
